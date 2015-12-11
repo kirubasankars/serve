c0b627fa034f9f24df7e9b7b3c444f2433469771
kirubasankars <kiruba.sankars@gmail.com>
2015-12-11 05:02:10 -0500

    added metal

--- main.go
+++ main.go
@@ -1,10 +1,13 @@
 package main
 
 import (	
-	"gravity/serve/server"
+	"github.com/serve/server"
+	"github.com/serve/metal"
 )
 
 func main() {
+	var m = metal.NewMetal();
+	_ = m
 	var s = new(server.Server);
 	s.SetConfig("C:/workspace/go/bin", "3000")
 	s.Start()
