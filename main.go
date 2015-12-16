ed7934a27c7039133a85692b44bbaa4701327928
kirubasankars <kiruba.sankars@gmail.com>
2015-12-15 19:57:29 -0500

    api path added

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 	"path/filepath"
 )
 
-func main() {
+func main() {	
 	dir, err := filepath.Abs(filepath.Dir(os.Args[0]))
 	if err != nil {
 		log.Fatal(err)
--- server/server.go
+++ server/server.go
@@ -5,13 +5,16 @@ import (
 	"io/ioutil"
 	"fmt"	
 	"os"
+	"html"
+	"github.com/gravity"
+	"strings"
 )
 
 type Server struct {
 	path string
 	port string
 }
-
+	
 type Site struct {
 	name string
 	path string
@@ -30,27 +33,38 @@ func exists(path string) (bool, error) {
     return true, err
 }
 
+func HandleApi(w http.ResponseWriter, r *http.Request) {	
+	paths := strings.Split(r.URL.Path, "/api")
+	v := gravity.Get(strings.ToLower(r.Method) ,html.EscapeString(paths[1]))
+	fmt.Fprintf(w, "Hello, %q", v)
+}
+
 func (s *Server) Start() {
 	sites, _ := ioutil.ReadDir(s.path + "/sites")
     for _, f := range sites {
 		if f.IsDir() {
+			var siteDir http.Handler
+			
 			siteRoot := "/" + f.Name() + "/"
 			site := new(Site)
 			site.name = f.Name()
 			site.path = s.path + "/sites" + siteRoot
 			site.uri = siteRoot
-			var r, _ = exists(site.path + "/app")
-			var siteDir http.Handler
-			fmt.Println("http://localhost:" + s.port + site.uri)
+			r, _ := exists(site.path + "/app")
+						
 			if(r) {
 				siteDir = http.FileServer(http.Dir(site.path + "/app"))
 			} else {
 				siteDir = http.FileServer(http.Dir(site.path))
 			}
-			http.Handle(site.uri, http.StripPrefix(site.uri, siteDir))
 			
+			http.HandleFunc(site.uri + "api/", HandleApi)
+			http.Handle(site.uri, http.StripPrefix(site.uri, siteDir))						
+			
+			fmt.Println("http://localhost:" + s.port + site.uri)
 		}
     }
+	
 	if err := http.ListenAndServe(":" + s.port, nil); err != nil {
 		fmt.Println("unable to start server")
 		fmt.Println(err)
