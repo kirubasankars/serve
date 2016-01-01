85cd2e20067aef4ddaf438333f2f24beaeffcc02
kirubasankars <kiruba.sankars@gmail.com>
2016-01-01 07:27:55 -0500

    removed database related code.

--- model.go
+++ model.go
@@ -9,14 +9,9 @@ import (
 func (site *Site) GetModel(path string) *metal.Metal {
 	model := metal.NewMetal()
 	var apiPath = site.path + "/api" + path
-	if data := readContent(apiPath + ".sql"); data != nil {
-		getData(string(data), model)
-		return model
-	} else {
-		var data = readContent(apiPath + ".json")
-		model.Parse(data)
-		return model
-	}
+	var data = readContent(apiPath + ".json")
+	model.Parse(data)
+	return model
 }
 
 func readContent(path string) []byte {
