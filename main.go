fa0a811ea1dfbe61df5752828281bf63ee8405b1
Kiruba Sankar Swaminathan <kiruba.sankars@gmail.com>
2016-01-12 16:22:29 -0500

    Update main.go

--- main.go
+++ main.go
@@ -1,19 +1,34 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"os"
 	"path/filepath"
+	"strconv"
+
+	"serve/serve"
 )
 
+var port = flag.Int("port", 3000, "port number to listen")
+var path = flag.String("path", DefaultPath(), "www folder path")
+
+func DefaultPath() string {
+	c, _ := filepath.Abs(filepath.Dir(os.Args[0]))
+	return c
+}
+
 func main() {
+	flag.Parse()
+
 	dir, err := filepath.Abs(filepath.Dir(os.Args[0]))
 	if err != nil {
 		log.Fatal(err)
 		return
 	}
 
-	var s = new(Server)
-	s.SetConfig(dir, "3000")
-	s.Start()
+	var server = new(serve.Server)
+	server.IO = new(FileIO)
+	server.SetConfig(dir, strconv.Itoa(*port))
+	server.Start()
 }
