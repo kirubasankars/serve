57d7f21371473e9b2cbf497c239ebe1119674cb9
KIRUBA SANKAR SWAMINATHAN <kiruba.sankars@gmail.com>
2016-08-15 06:17:57 -0400

    support for namespace, app and modules.

--- README.md
+++
@@ -1,4 +0,0 @@
-# serve
-
-
-Go based web server for web development
--- model.go
+++
@@ -1,8 +0,0 @@
-package serve
-
-import "github.com/kirubasankars/metal"
-
-func (site *Site) Model(path string, method string, input *metal.Metal) *metal.Metal {
-	server := site.server
-	return server.IO.API(site, path, method, input)
-}
