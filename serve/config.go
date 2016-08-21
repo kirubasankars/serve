ca948e404340f35e6125db42da78b6a60030eeb8
KIRUBA SANKAR SWAMINATHAN <kiruba.sankars@gmail.com>
2016-08-20 23:33:57 -0400

    config struct added to project

---
+++ serve/config.go
@@ -0,0 +1,18 @@
+package serve
+
+// NamespaceConfigration config for namespace object
+type NamespaceConfigration struct {
+	Modules []string
+	Roles   map[string][]string
+}
+
+// ApplicationConfigration config for application object
+type ApplicationConfigration struct {
+	Modules []string
+	Roles   map[string][]string
+}
+
+// ModuleConfigration config for module object
+type ModuleConfigration struct {
+	Permissions map[string][]string
+}
