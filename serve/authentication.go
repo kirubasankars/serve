a32fe0b6e916ed71a09a1b17157512ae4fb4d95f
Kiruba Sankar Swaminathan <kiruba.sankars@gmail.com>
2016-08-27 18:36:04 -0400

    authorization check in

--- serve/authentication.go
+++ serve/authentication.go
@@ -4,9 +4,37 @@ package serve
 type Authentication struct{}
 
 // Authenticate authenticate with username and password
-func (auth *Authentication) Authenticate(username string, password string, clientID string, clientSecret string) bool {
-	if username == "admin" && password == "admin" && clientID == "client_id" && clientSecret == "client_secret" {
-		return true
+func (auth *Authentication) Authenticate(param AuthParameter) int {
+	var count int
+
+	if param.refreshToken == "refresh_token" {
+		count++
 	}
-	return false
+	if param.authorizationCode == "code" {
+		count++
+	}
+	if param.username == "admin" && param.password == "admin" {
+		count++
+	}
+	if param.clientID == "client_id" && param.redirectURI == "url" {
+		count++
+	}
+	if param.clientID == "client_id" && param.clientSecret == "client_secret" {
+		count++
+	}
+
+	return count
+}
+
+// AuthParameter authParameter
+type AuthParameter struct {
+	refreshToken      string
+	authorizationCode string
+
+	clientID     string
+	redirectURI  string
+	clientSecret string
+
+	username string
+	password string
 }
